a67d13c9e3a93f04556e1860056516962abb226f
Maximiliano Cesar Nivoli <m_nivoli@hotmail.com>
2012-06-11 16:19:02 -0300

    maquetado

--- application/views/includes/header_v.php
+++ application/views/includes/header_v.php
@@ -1,6 +1,12 @@
-<html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="es">
 <head>
-	<title></title>
+	<title>Soluciones JS</title>
+	<link rel="stylesheet" href="<?=base_url();?>css/reset.css">
+	<link rel="stylesheet" href="<?=base_url();?>css/css.css">
 </head>
 <body>
+	<div id="cuerpo">
+		<div id="header">asdfasdf</div>
 
