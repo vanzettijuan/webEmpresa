a4754e71eb05327f2fc042b6e3ab2c5cc8f22ecd
vanzetti, juan <jjvanzetti@yahoo.com.ar>
2012-08-31 16:05:21 -0300

    3

--- application/views/inicio_v.php
+++ application/views/inicio_v.php
@@ -10,8 +10,8 @@
   	Además JS ofrece la posibilidad de personalizar las distintas aplicaciones teniendo como premisa principal las necesidades del usurario. Se brinda también la combinación de aplicaciones de escritorio con un sitio web dinámico, permitiendo de esta forma la independencia del cliente para diseñarlo con la información que desea y al instante en que la necesita. </p>
  </div>
  
-<div id="cajaSlider">
- <div class="bx-wrapper">
+  <div id="cajaSlider">
+     <div class="bx-wrapper">
 	  <ul id="slider1" >
 		  	<li>
 		      <img src="<?=$ruta?>img/t1.jpg">
